60e0d9692b40f03c467f2416e2e8cb9bbd6b0a65
SaifVelly <mohammed.kssim20022002@gmail.com>
2025-03-30 12:54:06 +0000

    User -> graphql

---
+++ src/main/java/com/rest/server/graphql/resolvers/UserMutationResolver.java
@@ -0,0 +1,58 @@
+package com.rest.server.graphql.resolvers;
+
+import com.rest.server.models.User;
+import com.rest.server.graphql.inputs.UserInput;
+import com.rest.server.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.stereotype.Controller;
+
+import java.time.Instant;
+
+@Controller
+public class UserMutationResolver {
+
+    @Autowired
+    private UserService userService;
+
+    @MutationMapping
+    public User createUser(@Argument UserInput input) {
+        User user = new User();
+        user.setUserTitle(input.getUserTitle());
+        user.setUserFirstName(input.getUserFirstName());
+        user.setUserLastName(input.getUserLastName());
+        user.setUserGender(input.getUserGender());
+        user.setUserEmail(input.getUserEmail());
+        user.setUserDateOfBirth(input.getUserDateOfBirth());
+        user.setUserRegisterDate(Instant.now().toString());
+        user.setUserPhone(input.getUserPhone());
+        user.setUserPicture(input.getUserPicture());
+        user.setUserLocationId(input.getUserLocationId());
+
+        return userService.createUser(user);
+    }
+
+    @MutationMapping
+    public User updateUser(@Argument String id, @Argument UserInput input) {
+        User user = new User();
+        user.setUserTitle(input.getUserTitle());
+        user.setUserFirstName(input.getUserFirstName());
+        user.setUserLastName(input.getUserLastName());
+        user.setUserGender(input.getUserGender());
+        user.setUserEmail(input.getUserEmail());
+        user.setUserDateOfBirth(input.getUserDateOfBirth());
+        user.setUserPhone(input.getUserPhone());
+        user.setUserPicture(input.getUserPicture());
+        user.setUserLocationId(input.getUserLocationId());
+        user.setUserRegisterDate(Instant.now().toString());
+
+        return userService.updateUser(id, user);
+    }
+
+    @MutationMapping
+    public String deleteUser(@Argument String id) {
+        userService.deleteUser(id);
+        return id;
+    }
+}
---
+++ src/main/java/com/rest/server/graphql/resolvers/UserQueryResolver.java
@@ -0,0 +1,35 @@
+package com.rest.server.graphql.resolvers;
+
+import com.rest.server.models.User;
+import com.rest.server.graphql.inputs.UserPage;
+import com.rest.server.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserQueryResolver {
+
+    @Autowired
+    private UserService userService;
+
+    @QueryMapping
+    public UserPage getAllUsers(@Argument int page, @Argument int size) {
+        Page<User> users = userService.allUsers(PageRequest.of(page, size));
+        return new UserPage(users.getContent(), users.getTotalElements(), page, size);
+    }
+
+    @QueryMapping
+    public User getUser(@Argument String id) {
+        return userService.singleUser(id).orElse(null);
+    }
+
+    @QueryMapping
+    public UserPage searchUsers(@Argument String query, @Argument int page, @Argument int size) {
+        Page<User> users = userService.searchUsers(query, PageRequest.of(page, size));
+        return new UserPage(users.getContent(), users.getTotalElements(), page, size);
+    }
+}
